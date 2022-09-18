bdcfaa93860dadebb6eccd6cd2a30b5f49f6b962
DiscoIoR <rmfield@qq.com>
2022-09-18 14:14:28 +0800

    first commit

---
+++ src/assets/js/SlideshowBackground.js
@@ -0,0 +1,55 @@
+export function slideshowBackground(){
+	let settings = {
+		images: {
+			'src/assets/images/bg01.jpg': 'left 40%',
+			'src/assets/images/bg02.jpg': 'left 40%',
+			'src/assets/images/bg03.jpg': 'left 40%',
+			'src/assets/images/bg04.jpg': 'left 20%'
+		},
+		delay: 6000
+	};
+
+	let pos = 0, lastPos = 0,
+		$wrapper, $bgs = [], $bg,
+		k, v;
+
+	$wrapper = document.querySelector('#bg');
+
+
+	for (k in settings.images) {
+		$bg = document.createElement('div');
+		$bg.style.backgroundImage = 'url("' + k + '")';
+		$bg.style.backgroundPosition = settings.images[k];
+		$bg.style.backgroundSize = 'cover';
+		$bg.style.backgroundRepeat = 'no-repeat';
+		$wrapper.appendChild($bg);
+		$bgs.push($bg);
+	}
+	console.log($wrapper)
+
+	// Main loop.
+	$bgs[pos].classList.add('visible');
+	$bgs[pos].classList.add('top');
+
+
+	window.setInterval(function () {
+
+		lastPos = pos;
+		pos++;
+
+		// Wrap to beginning if necessary.
+		if (pos >= $bgs.length)
+			pos = 0;
+
+		// Swap top images.
+		$bgs[lastPos].classList.remove('top');
+		$bgs[pos].classList.add('visible');
+		$bgs[pos].classList.add('top');
+
+		// Hide last image after a short delay.
+		window.setTimeout(function () {
+			$bgs[lastPos].classList.remove('visible');
+		}, settings.delay / 2);
+
+	}, settings.delay);
+}
---
+++ src/views/IndexView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="main">
+    <header id="header">
+      <h1>Rmfield</h1>
+      <p>A personal site.<br/>
+        Maintained by <a href="https://github.com/DiscoIoR">DiscoloR</a>.</p>
+    </header>
+
+    <form id="signup-form" method="post" action="#">
+      <input type="text" name="email" id="username" placeholder="Username"/>
+      <input type="password" name="email" id="password" placeholder="Password"/>
+      <button type="button">Sign Up</button>
+    </form>
+
+    <footer id="footer">
+<!--#gitignoreline_start-->
+<!--#gitignoreline_end-->
+    </footer>
+  </div>
+
+	<div id="bg"></div>
+</template>
+
+<script>
+import "../assets/css/index.css"
+import {onMounted} from "vue";
+import {slideshowBackground} from "../assets/js/SlideshowBackground";
+
+export default {
+	name: "",
+	setup() {
+
+
+
+    onMounted(()=>{
+      slideshowBackground()
+    })
+
+		return {
+			
+		}
+	}
+}
+
+</script>
+
+
