cf9e547369f699ce3d0b84a1d601d8e4a1fe3507
DiscoIoR <rmfield@qq.com>
2022-09-25 22:48:34 +0800

    新增方舟页面导航

--- src/views/ArknightsView.vue
+++ src/views/ArknightsView.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="back">
-    <div class="include">
+    <div class="include mid-layer">
       <router-view/>
+    </div>
+    <div class="ak-navigation mid-layer">
+      <router-link to="/arknights/general" class="ak-route-button left-button">概览</router-link>
+      <router-link to="/arknights/gacha" class="ak-route-button">寻访</router-link>
+      <router-link to="/arknights/diamond" class="ak-route-button">源石</router-link>
+      <router-link to="/arknights/order" class="ak-route-button right-button">充值</router-link>
     </div>
   </div>
 </template>
@@ -13,46 +19,5 @@ export default {
 </script>
 
 <style scoped>
-.back{
-  height: 100%;
-  width: 100%;
-  background-image: url("../assets/images/arknights/bg01.jpg");
-  background-position: center;
-  background-size: cover;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-.include{
-  background-color: rgba(0,0,0,0.3);
-  backdrop-filter: blur(3px);
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: flex-start;
-  max-height: 80%;
-  padding: 0.7em 0.4em 0.7em 0.4em;
-  width: 59.4em;
-  overflow-y: auto;
-}
-@media (max-width: 960px){
-  .include{
-    width: 39.6em;
-  }
-}
-@media (max-width: 650px){
-  .include{
-    width: 19.8em;
-  }
-}
-*::-webkit-scrollbar{
-  width: 5px;
-  height: 5px;
-}
-*::-webkit-scrollbar-thumb {
-  border-radius: 3px;
-  background-color: rgba(255,255,255,0.5);
-}
+@import "../assets/css/arknights/akIndex.css";
 </style>
